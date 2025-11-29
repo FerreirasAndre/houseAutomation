3851b5e97279cb6e0e4d11bb62b21001db00b851
davijvasconcellos-senai <davijvasconcellos@gmail.com>
2025-11-28 21:13:59 -0300

    Subindo o código do display

--- DigitalReadSerial/DigitalReadSerial.ino
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -1,3 +1,6 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
   int led1 = 3;
   int led2 = 4;
@@ -8,13 +11,21 @@
 void setup() {
   Serial.begin(9600);
 
+//módulo lampadas
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
   pinMode(led5, OUTPUT);
+
+//módulo display
+
+lcd.begin(16, 2);
+lcd.clear();
+lcd.setCursor(0, 0);
 }
 
 void loop() {
   lampadas();
+  display();
 }
---
+++ DigitalReadSerial/display.ino
@@ -0,0 +1,7 @@
+
+
+void display(){
+  lcd.print("Seja Bem Vindo");
+  lcd.setCursor(0, 1);
+  lcd.print("Casa Inteligente");
+}
